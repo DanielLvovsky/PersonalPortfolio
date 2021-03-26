e0c0db5fb872e9b2e2af65aafb7edd6971f55a39
Daniel Lvovsky <54752143+DanielLvovsky@users.noreply.github.com>
2021-03-26 10:53:05 -0700

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite-plugin-windicss'
+// @ts-expect-error
+import typography from 'windicss/plugin/typography'
+
+export default defineConfig({
+  darkMode: 'class',
+  plugins: [
+    typography(),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: '"Inter", Inter var,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
+      },
+      typography: {
+        DEFAULT: {
+          css: [{
+            color: 'var(--fg)',
+            a: {
+              color: 'var(--fg-deeper)',
+              textDecoration: 'none',
+            },
+            b: { color: 'var(--fg-deep)' },
+            code: { color: 'var(--fg-deep)' },
+            strong: { color: 'var(--fg-deep)' },
+            em: { color: 'inherit' },
+            h1: { color: 'var(--fg-deeper)' },
+            h2: { color: 'var(--fg-deep)' },
+            h3: { color: 'inherit' },
+            h4: { color: 'inherit' },
+            blockquote: { color: 'inherit' },
+          }],
+        },
+      },
+    },
+  },
+})
